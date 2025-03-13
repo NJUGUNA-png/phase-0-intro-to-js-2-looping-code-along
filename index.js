f3c37423136214a94813d72ddb5a950d9976d06d
NJUGUNA NJOROGE <engbrian01@gmail.com>
2025-03-14 00:29:48 +0300

    added changes

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const messages = [];
+function writeCards(name, eventName) {
+	for (let i = 0; i < name.length; i++) {
+		messages[i] = `Thank you, ${name[i]}, for the wonderful ${eventName} gift!`;
+	}
+	return messages;
+}
+
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+console.log(messages);
+
+function countDown(number) {
+	while (number >= 0) {
+		console.log(number--)
+	}
+}
+
+countDown(10)
